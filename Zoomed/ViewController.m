625e82260d4b828ecf0883bc05573bd30f7c662b
jungao <jungao@staff.weibo.com>
2020-11-03 10:57:49 +0800

    feature：添加图片显示和按钮

--- Zoomed/ViewController.m
+++ Zoomed/ViewController.m
@@ -6,10 +6,14 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+#import <AFNetworking.h>
+#import <UIImageView+AFNetworking.h>
 
 @interface ViewController ()
 
-
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIButton *button;
 
 @end
 
@@ -19,7 +23,53 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
+    [self viewsLayoutInit];
 }
 
+#pragma mark - views layout init
+- (void)viewsLayoutInit
+{
+    [self.view addSubview:self.imageView];
+    [self.view addSubview:self.button];
+
+    [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.trailing.equalTo(self.view).offset(10);
+        make.bottomMargin.equalTo(self.view);
+//        make.height
+    }];
+    [self.button mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.topMargin.equalTo(self.view);
+        make.centerX.equalTo(self.view.mas_centerX);
+    }];
+}
+
+#pragma mark - buttons actions
+- (void)buttonAction:(UIButton *)sender
+{
+    [self.imageView setImageWithURL:[NSURL URLWithString:@"https://wx3.sinaimg.cn/large/49535fefly1fj72buwho5j21hc0u00xy.jpg"]];
+}
+
+#pragma mark -
+- (UIImageView *)imageView
+{
+    if (!_imageView)
+    {
+        _imageView = [[UIImageView alloc] init];
+        _imageView.contentMode = UIViewContentModeScaleAspectFit;
+    }
+    return _imageView;
+}
+
+- (UIButton *)button
+{
+    if (!_button)
+    {
+        _button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+        [_button setTitle:@"加载图片" forState:UIControlStateNormal];
+        [_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    }
+    return _button;
+}
 
 @end
