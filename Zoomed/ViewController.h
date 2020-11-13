c5f3a81adcd993d0362b4813ca0473d94388dce7
jungao <jungao@staff.weibo.com>
2020-11-13 19:32:28 +0800

    添加注释

--- Zoomed/ViewController.h
+++ Zoomed/ViewController.h
@@ -11,7 +11,24 @@ typedef void (^SafeAreaBlock)(CGFloat topSafeHeight,CGFloat bottomSafeHeight,CGF
 
 @interface ViewController : UIViewController
 
+/// 在这个block实现安全区附近的UIView布局
 @property (nonatomic, copy) SafeAreaBlock safeAreaBlock NS_DEPRECATED_IOS(9_0, 16_0, "请使用自动布局");
+
+
+/// 继承自ViewController的类，在实现这个方法的时候，必须先调用父方法
+/// [super viewDidLayoutSubviews];
+- (void)viewDidLayoutSubviews;
+
+///  继承自ViewController的类，在实现这个方法的时候，必须先调用父方法
+/// [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+/// @param keyPath keyPath
+/// @param object object
+/// @param change change
+/// @param context context
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey, id> *)change
+                       context:(void *)context;
 
 @end
 
