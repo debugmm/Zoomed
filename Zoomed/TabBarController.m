3217d22a7578e91d157376018bd06d3f59fcedc8
jungao <jungao@staff.weibo.com>
2020-11-16 11:39:58 +0800

    注释其他页面

--- Zoomed/TabBarController.m
+++ Zoomed/TabBarController.m
@@ -40,19 +40,19 @@
     v1.tabBarItem.title = @"checked";
     v1.tabBarItem.image = [UIImage imageNamed:@"checked"];
 
-    NavigationController *v2 = [NavigationController testNavigationControllerWithTitle:@"identity"];//[[TestViewController alloc] init];
-    v2.tabBarItem.title = @"identity";
-    v2.tabBarItem.image = [UIImage imageNamed:@"identity"];
+//    NavigationController *v2 = [NavigationController testNavigationControllerWithTitle:@"identity"];//[[TestViewController alloc] init];
+//    v2.tabBarItem.title = @"identity";
+//    v2.tabBarItem.image = [UIImage imageNamed:@"identity"];
+//
+//    NavigationController *v3 = [NavigationController testNavigationControllerWithTitle:@"location"];//[[TestViewController alloc] init];
+//    v3.tabBarItem.title = @"location";
+//    v3.tabBarItem.image = [UIImage imageNamed:@"location"];
+//
+//    NavigationController *v4 = [NavigationController testNavigationControllerWithTitle:@"refresh"];//[[TestViewController alloc] init];
+//    v4.tabBarItem.title = @"refresh";
+//    v4.tabBarItem.image = [UIImage imageNamed:@"refresh"];
 
-    NavigationController *v3 = [NavigationController testNavigationControllerWithTitle:@"location"];//[[TestViewController alloc] init];
-    v3.tabBarItem.title = @"location";
-    v3.tabBarItem.image = [UIImage imageNamed:@"location"];
-
-    NavigationController *v4 = [NavigationController testNavigationControllerWithTitle:@"refresh"];//[[TestViewController alloc] init];
-    v4.tabBarItem.title = @"refresh";
-    v4.tabBarItem.image = [UIImage imageNamed:@"refresh"];
-
-    self.viewControllers = @[v1,v2,v3,v4];
+    self.viewControllers = @[v1];//@[v1,v2,v3,v4];
 }
 
 @end
